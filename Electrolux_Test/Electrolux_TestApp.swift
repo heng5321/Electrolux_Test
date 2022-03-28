64e2ef221d25bb487d48e8163cf29fee3d31a43e
heng5321 <heng_kkh5321@hotmail.com>
2022-03-28 23:47:55 +0800

    Init Commit Project
    -Init Commit Project

---
+++ Electrolux_Test/Electrolux_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electrolux_TestApp.swift
+//  Electrolux_Test
+//
+//  Created by koh kar heng on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Electrolux_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Electrolux_TestUITests/Electrolux_TestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Electrolux_TestUITestsLaunchTests.swift
+//  Electrolux_TestUITests
+//
+//  Created by koh kar heng on 28/03/2022.
+//
+
+import XCTest
+
+class Electrolux_TestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
